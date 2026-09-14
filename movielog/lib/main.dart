e8f8978bed0bc0f331b4c709bc5b8458e8cda734
soyeon <happyksy0501@gmail.com>
2026-09-14 12:42:55 +0900

    chore: Flutter 프로젝트를 생성한다

---
+++ movielog/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MovieLogApp());
+}
+
+class MovieLogApp extends StatelessWidget {
+  const MovieLogApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Movielog',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // TRY THIS: Try running your application with "flutter run". You'll see
+        // the application has a purple toolbar. Then, without quitting the app,
+        // try changing the seedColor in the colorScheme below to Colors.green
+        // and then invoke "hot reload" (save your changes or press the "hot
+        // reload" button in a Flutter-supported IDE, or press "r" if you used
+        // the command line to start the app).
+        //
+        // Notice that the counter didn't reset back to zero; the application
+        // state is not lost during the reload. To reset the state, use hot
+        // restart instead.
+        //
+        // This works for code too, not just values: Most code changes can be
+        // tested with just a hot reload.
+        useMaterial3: true,
+      ),
+      home: const StartScreen(),
+    );
+  }
+}
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            const Text('FlUTTER 0주차'),
+            const SizedBox(height: 64),
+            const Icon(
+              Icons.movie_outlined,
+              size: 72,
+              color: Colors.deepPurple,
+            ),
+            const SizedBox(height: 72),
+            const Text(
+              '영화의 순간을 \n기록하세요',
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              style: TextStyle(fontSize: 24, height: 1.4),
+            ),
+            const SizedBox(height: 8),
+            const Text(
+              '보고 싶은 영화부터 나만의 평점까지\n 한곳에서 관리해요',
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 80),
+            ElevatedButton(
+              onPressed: () {
+                debugPrint('시작하기 버튼을 눌렀습니다.');
+              },
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size(double.infinity, 56),
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                backgroundColor: Colors.deepPurple,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              child: const Text('시작하기', style: TextStyle(color: Colors.white)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
